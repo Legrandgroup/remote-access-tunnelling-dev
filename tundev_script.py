35341257acfd7665de4b2a2ff8530e4ed415947c
Lionel AINS <lionel.ains@legrand.fr>
2015-03-30 16:43:31 +0200

    Adapting variables to prototype RDV server

--- tundev_script.py
+++ tundev_script.py
@@ -6,13 +6,12 @@ from __future__ import print_function
 
 import paramiko
 
-class TunnellingDev():
+class TunnellingDev(object):
     """ Class representing a tunnelling device
     A tunnelling device is a abstract device gathering client devices or server devices
     """
     
-    PROTO_RDV_SERVER = 'localhost'
-    #~ PROTO_RDV_SERVER = '10.10.8.41'
+    PROTO_RDV_SERVER = '10.10.8.11'
     
     def __init__(self):
         self._rdv_server = TunnellingDev.PROTO_RDV_SERVER
@@ -21,8 +20,8 @@ class TunnellingDev():
     def rdv_server_connect(self):
         """ Initiate the ssh connection to the RDV server """
         self._ssh_connection = paramiko.SSHClient()
-        self._ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self._ssh_connection.connect(self._rdv_server, username='cl0001', password='cl0001')
+        self._ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # FIXME: really check the server certificate
+        self._ssh_connection.connect(self._rdv_server, username='rpi0001', password='cl0001')
 
     def rdv_server_disconnect(self):
         """ Close the ssh connection to the RDV server if it is up """
