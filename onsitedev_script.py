43a2161081d49d3da33b1f47802d5a663cf30c43
Lionel AINS <lionel.ains@legrand.fr>
2015-03-31 10:44:13 +0200

    Adding initial ssh connection expect script

--- onsitedev_script.py
+++ onsitedev_script.py
@@ -4,33 +4,68 @@
 
 from __future__ import print_function
 
+import os
+import sys
+
 import tundev_script
+import argparse
+import logging
+
+progname = os.path.basename(sys.argv[0])
 
 class OnsiteDev(tundev_script.TunnellingDev):
     """ Script to connect to a RDV server from an onsite dev """
 
-    def __init__(self):
-        super(OnsiteDev, self).__init__()
+    def __init__(self, username, key_filename, logger):
+        super(OnsiteDev, self).__init__(username=username, key_filename=key_filename, logger=logger)
 
     def run_cmd(self, command):
         """ Execute this command on the remote server """
         if command:
             if self._ssh_connection:
                 print('Host: %s'  % (self._rdv_server))
-                stdin, stdout, stderr = self._ssh_connection.exec_command(command)
-                stdin.close()
-                print('Dumping output...\n')
-                for line in stdout.read().splitlines():
-                    print('host: %s: %s' % (self._rdv_server, line))
-                print('...Done\n')
+                #stdin, stdout, stderr = self._ssh_connection.exec_command(command)
+                #stdin.close()
+                #print('Dumping output...\n')
+                #for line in stdout.read().splitlines():
+                #    print('host: %s: %s' % (self._rdv_server, line))
+                #print('...Done\n')
+                channel = self._ssh_connection.invoke_shell()
+                channel.send('echo bla\r')
+                result = ''
+                while channel.recv_ready():
+                    result += channel.recv(1024)
+                print('Got output:' + result)
 
     def exit(self):
         """ Terminate the onsite dev script """
         self.rdv_server_disconnect()
 
 if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser(description="This program automatically connects to a RDV server. \
+and automates the typing of tundev shell commands from the tunnelling devices side in order to setup a tunnel session", prog=progname)
+    parser.add_argument('-d', '--debug', action='store_true', help='display debug info', default=False)
+    args = parser.parse_args()
+
+    # Setup logging
+    logging.basicConfig()
+    
+    logger = logging.getLogger(__name__)
+    
+    if args.debug:
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.INFO)
+    
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("%(levelname)s %(asctime)s %(name)s():%(lineno)d %(message)s"))
+    logger.addHandler(handler)
+    logger.propagate = False
+    
+    logger.debug(progname + ": Starting")
     print('Starting onsite dev script');
-    onsite_dev = OnsiteDev()
+    onsite_dev = OnsiteDev(username = 'rpi1001', key_filename = '/home/lionel/.ssh/id_rsa', logger = logger)
     onsite_dev.rdv_server_connect()
     onsite_dev.run_cmd('echo bla')
     onsite_dev.exit()
--- tundev_script.py
+++ tundev_script.py
@@ -5,6 +5,21 @@
 from __future__ import print_function
 
 import paramiko
+import pexpect
+import sys
+
+import logging
+
+# def sigwinch_passthrough (sig, data):
+#     # Check for buggy platforms (see pexpect.setwinsize()).
+#     if 'TIOCGWINSZ' in dir(termios):
+#         TIOCGWINSZ = termios.TIOCGWINSZ
+#     else:
+#         TIOCGWINSZ = 1074295912 # assume
+#     s = struct.pack ("HHHH", 0, 0, 0, 0)
+#     a = struct.unpack ('HHHH', fcntl.ioctl(sys.stdout.fileno(), TIOCGWINSZ , s))
+#     global global_pexpect_instance
+#     global_pexpect_instance.setwinsize(a[0],a[1])
 
 class TunnellingDev(object):
     """ Class representing a tunnelling device
@@ -13,15 +28,67 @@ class TunnellingDev(object):
     
     PROTO_RDV_SERVER = '10.10.8.11'
     
-    def __init__(self):
+    def __init__(self, username, key_filename, logger):
+        """
+        \param logger A logging.Logger to use for log messages
+        """
         self._rdv_server = TunnellingDev.PROTO_RDV_SERVER
         self._ssh_connection = None
-
+        self._ssh_username = username
+        self._ssh_key_filename = key_filename
+        self._exp = None
+        self._prompt = '1001[$] '
+        self.logger = logger
+    
     def rdv_server_connect(self):
         """ Initiate the ssh connection to the RDV server """
-        self._ssh_connection = paramiko.SSHClient()
-        self._ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # FIXME: really check the server certificate
-        self._ssh_connection.connect(self._rdv_server, username='rpi0001', password='cl0001')
+        self._exp = pexpect.spawn('ssh', ['-oUserKnownHostsFile=/dev/null', '-oStrictHostKeyChecking=no', self._ssh_username + '@' + self._rdv_server])
+        supposedly_logged_in = False
+        surely_logged_in = False
+        
+        index = self._exp.expect([pexpect.TIMEOUT, 'Permission denied', self._ssh_username + '@.*password: ', self._prompt], timeout=4)
+        if self.logger.isEnabledFor(logging.DEBUG):
+            self._exp.logfile = sys.stdout    # Log to stdout in DEBUG mode
+        
+        if index == 0:
+            if self._exp.isalive():
+                self.logger.info("Supposedly logged in...")
+                if self._exp.buffer:
+                    self.logger.info("Input buffer is:\n'" + self._exp.buffer + "'")
+                self._exp.logfile = None    # Do not log to console anymore , interact will do it
+                supposedly_logged_in = True
+            else:
+                self.logger.error("Remote connection closed")
+                exit(5)
+        elif index == 1:
+            self.logger.error("Permission denied while logging in")
+            raise Exception('PermissionDenied')
+        elif index == 2:
+            self.logger.error("Incorrect password")
+            raise Exception('BadPassword')
+        elif index == 3:    # linux_prompt_catchall_regexp
+            supposedly_logged_in = True    # We are probably logged in
+        
+        if supposedly_logged_in: print('Supposedly logged in')
+    
+        if supposedly_logged_in:    # If we think we have been logged in... try to hit return and check if we have a prompt once more
+            self._exp.send('\r')    # Wake up terminal (we send a carriage return to find out if we are logged in (in a shell) or not (in the prompt))
+            index = self._exp.expect([pexpect.TIMEOUT, pexpect.EOF, self._prompt], timeout=2)
+            if index == 0:    # Timeout
+                logger.error("Could not wake up terminal")
+                exit(5)
+            elif index == 1:    # EOF
+                logger.error("Remote connection closed")
+                exit(5)
+            elif index == 2:    # linux_prompt_catchall_regexp a second time
+                surely_logged_in = True    # We really think we are logged in now
+        if surely_logged_in: print('Surely logged in')
+            
+#     def rdv_server_connect(self):
+#         """ Initiate the ssh connection to the RDV server """
+#         self._ssh_connection = paramiko.SSHClient()
+#         self._ssh_connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())  # FIXME: really check the server certificate
+#         self._ssh_connection.connect(self._rdv_server, username=self._ssh_username, key_filename=self._ssh_key_filename)
 
     def rdv_server_disconnect(self):
         """ Close the ssh connection to the RDV server if it is up """
